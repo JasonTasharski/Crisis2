8316d2c0db3b310745d60500b82aaafb3e1ea4bb
J T <jtasharski@gmail.com>
2015-09-10 02:21:55 -0700

    fuck callbacks

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,11 +1,14 @@
 var app = angular.module('crisisApp.controllers', ['ngRoute', 'ngResource']);
 
+
 app.controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
+
 	socket.on('allRooms', function(data){
 		$scope.$apply(function(){
 			$scope.allRooms = data;
 		});
 	});
+
 	$scope.createRoom = function() {
 		var teamPick;
 		if (Math.random() > 0.5){
@@ -17,26 +20,32 @@ app.controller('MainCtrl', ['$scope', '$location', function ($scope, $location) 
 	  console.log("emit new room; waiting...");
 	  $location.url("/wait");
 	}
+
 	$scope.joinRoom = function(room) {
 		console.log(room);
 	  socket.emit('subscribe', room);
 	  $location.url("/wait");
 	}
+
 	socket.on('positiveConfirmation', function (data){
 		console.log("received positive confirmation");
 		currentUser = data.user;
 		clientScenario = data.scenario;
 		$location.url("/crisis");
 	});
+
 	socket.on('negativeConfirmation', function(){
 		console.log("received negative confirmation");
 		$location.url("/sorry");
 	});
 }]);
 
+
 app.controller('CrisisCtrl', ['$scope', '$route', '$location', function ($scope, $route, $location) {
+
 	$scope.playerName = currentUser.faction[0].leader;
 	$scope.actionList = currentUser.faction[0].actions;
+
 	$scope.leaveRoom = function(room) {
 		// resign; update room to finished, set ending to "user.team resignation", give other user that screen
 		// on server side, set room to users -= 1; if > 0, other user gets win message; if 0, delete room
@@ -45,21 +54,27 @@ app.controller('CrisisCtrl', ['$scope', '$route', '$location', function ($scope,
 	  socket.emit('unsubscribe', room);
 	  $location.url("/");
 	}
+
 	$scope.act = function(action){
 		console.log(action.impact);
 		socket.emit('action', {room: currentUser.room, impact: action.impact});
 		// set action to done, unavailable
 	}
+
 	socket.on('intel', function(data){
+		console.log("received intel");
+		console.log(data);
 		$scope.$apply(function(){
-			$scope.intel = data.content;
+			$scope.intel = data;
 		});
 	});
+
 	$scope.sendMessage = function(){
 		console.log(currentUser);
 		socket.emit('userMessage', {room: currentUser.room, message: {from: currentUser.faction[0].leader, content: $scope.msg}});
 		$scope.msg = "";
 	}
+
 	socket.on('allMessages', function(data){
 		$scope.$apply(function(){
 			console.log(data);
--- server.js
+++ server.js
@@ -34,6 +34,8 @@ var arbitrary = app.listen(process.env.PORT || 8080, function(){
   console.log("server listening ");
 });
 
+io = require('socket.io').listen(arbitrary);
+
 var pressureCeasefireU = new Action({available: true, done: false, text: "Push for Ceasefire!", details: "Pressure the friendly Ukrainian Government to enforce a ceasefire. Conflict will deescalate, but friendly forces will be vulnerable to a surprise attack.", impact: {influenceOne: -0.2, momentumOne: -0.5, escalation: -0.1}});
 var pressureCeasefireR = new Action({available: true, done: false, text: "Push for Ceasefire!", details: "Pressure the friendly Separatist Republics to enforce a ceasefire. Conflict will deescalate, but friendly forces will be vulnerable to a surprise attack.", impact: {influenceTwo: -0.2, momentumTwo: -0.5, escalation: -0.1}});
 var armUkraine = new Action({available: true, done: false, text: "Arm Ukraine!", details: "Send weapons to help the friendly Ukrainian Government win the war. Friendly forces will make more progress, but the conflict will escalate.", impact: {influenceOne: 0.2, momentumOne: 1, escalation: 0.2}});
@@ -48,9 +50,18 @@ var russia = new Team({title: "Russia", leader: "Vladimir Putin", regimeType: "a
 //var prc = new Team({title: "China", leader: "Xi Jinping", regimeType: "autocratic", strength: 7, baseApproval: 5, baseInfluence: 6}); //context for other values
 
 
-var ukraine = new Scenario({title: "Ukrainian Civil War", teamOne: usa, teamTwo: russia, introOne: "Heavily armed separatists have seized control of cities in Eastern Ukraine. We believe they are backed by the Russian government. We have to do something to prevent this country from being torn apart! The Ukrainian government is commencing an anti-terrorist operation.", introTwo: "The CIA has overthrown the Ukrainian government! We must act to protect the Russians in the east of the country before they're crushed by the coup leaders. After all, Ukraine was historically part of Russia anyway.", startParameters: new Situation({active: false, escalation: 0.3, balance: 5, momentumOne: 2, momentumTwo: 1.5, approvalOne: usa.baseApproval, approvalTwo: russia.baseApproval, influenceOne: usa.baseInfluence, influenceTwo: russia.baseApproval}), strengthOne: usa.strength, strengthTwo: russia.strength});
+var ukraine = new Scenario({title: "Ukrainian Civil War", teamOne: usa, teamTwo: russia, introOne: "Heavily armed separatists have seized control of cities in Eastern Ukraine. We believe they are backed by the Russian government. We have to do something to prevent this country from being torn apart! The Ukrainian government is commencing an anti-terrorist operation.", introTwo: "The CIA has overthrown the Ukrainian government! We must act to protect the Russians in the east of the country before they're crushed by the coup leaders. After all, Ukraine was historically part of Russia anyway.", startParameters: new Situation({active: false, escalation: 0.3, balance: 5, momentumOne: 2, momentumTwo: 1.5, approvalOne: usa.baseApproval, approvalTwo: russia.baseApproval, influenceOne: usa.baseInfluence, influenceTwo: russia.baseInfluence}), strengthOne: usa.strength, strengthTwo: russia.strength});
 
-var checkResults = function(scenario, situation){
+var updateSituation = function(target, impact, callback){
+	target.momentumOne += impact.momentumOne;
+	target.momentumTwo += impact.momentumTwo;
+	target.escalation += impact.escalation;
+	target.influenceOne += impact.influenceOne;
+	target.influenceTwo += impact.influenceTwo;
+	callback();
+}
+
+var checkResults = function(scenario, situation, callback){
 	if (scenario.title == "Ukrainian Civil War"){
 		if (situation.balance >= 10){
 			//Ukraine has suppressed the Russian-backed separatists
@@ -63,23 +74,16 @@ var checkResults = function(scenario, situation){
 	// on action, check for particular combinations of values in the situation (after the action is implemented) that fulfill victory or defeat conditions for either side. For instance, if Russia's influence is at any point higher than (equal to?) US influence, Putin smirks. If Russian approval ever reaches zero, Putin is overthrown (if US influence is high, Russia gains a new democratic government; if low, Russia falls into chaos). If escalation ever reaches zero, there's peace in Ukraine, and no one loses. If escalation ever exceeds ten, there's an accidental nuclear launch and everyone dies. If the balance ever reaches zero or ten, one side or the other wins the war completely, making their backer happy. Every action MUST have a timer, otherwise spam-to-victory is possible (timer is shorter when influence and approval are high, and longer when influence and approval are low)
 };
 
-io = require('socket.io').listen(arbitrary);
-
 io.on('connection', function(socket){
 
-  console.log("user connected"); //log
-  //emit rooms
-  //var allRooms;
+  console.log("user connected");
   Room.find(function(err, rooms){
   	socket.emit('allRooms', rooms);
   });
-  //console.log(allRooms);
-  //socket.emit('allRooms', allRooms);
 
-  // socket.join('roomOne'); // joins a specfic room; use a variable so that the user is joining a specific room
   socket.on('disconnect', function(){ //logs disconnect; sockets leave room automatically on disconnect; on disconnect
-	// find rooms where user is connected; set teams to "" if they match socket.id
-	console.log("disconnect initiated!");
+		// find rooms where user is connected; set teams to "" if they match socket.id
+		console.log("disconnect initiated!");
   	Room.findOne({oneFill: socket.id}, function(err, foundRoom){
   		if (foundRoom){
   			foundRoom.oneFill = "";
@@ -165,60 +169,44 @@ io.on('connection', function(socket){
  	  }
  	  //
 	});
-	socket.on('action', function(data){ //on action, update situation; when situation is done updating, emit updated situation and let the other stuff work itself out client-side
-		// increment situation values by by delivered impact
-		// then emit update to delivered room
-		// options:
-			// in room, based on team and situation, change intel
-			// emit intel with no distinction according to team
-		console.log("received action");
-		console.log(data.impact);
+
+	socket.on('action', function(data){
     Room.findOne({_id: data.room}, function(err, foundRoom){
     	console.log(foundRoom.situation[0]);
-   		foundRoom.situation[0].influenceOne += data.impact.influenceOne;
-   		foundRoom.situation[0].influenceTwo += data.impact.influenceTwo;
-   		foundRoom.situation[0].momentumOne += data.impact.momentumOne;
-   		foundRoom.situation[0].momentumTwo += data.impact.momentumTwo;
-   		foundRoom.situation[0].escalation += data.impact.escalation;
-   		//situation math!
-   		foundRoom.situation[0].balance += (foundRoom.situation[0].momentumOne - foundRoom.situation[0].momentumTwo);
-   		// end math!
-   		foundRoom.save();
-   		console.log(foundRoom.situation[0].balance);
-   		console.log("impact saved");
-   		var newIntel;
-   		//calculate
-   		// io.to(data.room).emit('intel', {}); //UNCOMMENT!!!
+    	updateSituation(foundRoom.situation[0], data.impact, function(){
+    		foundRoom.save(function(){
+    			io.to(data.room).emit('intel', foundRoom.situation[0]);
+    		});
+    	});
     });
-	// everything between here and the above comments will change
 	});
+
 	socket.on('unsubscribe', function(room) {
     console.log('leaving room');
     console.log(socket.id);
     socket.leave(room);
     Room.findOne({oneFill: socket.id}, function(err, foundRoom){
-  		if (foundRoom){
+  		if (foundRoom.users < 2){
+				foundRoom.remove();
+				console.log("room deleted");
+			} else if (foundRoom){
   			foundRoom.oneFill = "";
   			foundRoom.users--;
   			foundRoom.save();
-  			if (foundRoom.users == 0){
-  				foundRoom.remove();
-  				console.log("room deleted");
-  			}
   		}
   	});
   	Room.findOne({twoFill: socket.id}, function(err, foundRoom){
-  		if (foundRoom){
+  		if (foundRoom.users < 2){
+				foundRoom.remove();
+				console.log("room deleted");
+			} else if (foundRoom){
   			foundRoom.twoFill = "";
   			foundRoom.users--;
   			foundRoom.save();
-  			if (foundRoom.users == 0){
-  				foundRoom.remove();
-  				console.log("room deleted");
-  			}
   		}
   	});
 	});
+
 	socket.on('userMessage', function(data) {
   	console.log(data);
   	Room.findOne({_id: data.room}, function(err, foundRoom){
